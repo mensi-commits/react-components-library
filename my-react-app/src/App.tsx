a65eeff86169a0529798292da1a6cc7b677d1859
Mohamed Amine Mensi <mensimohamedamine25@gmail.com>
2026-05-14 23:36:27 +0100

    whatever

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,23 @@
-
-
-
-import Message from './components/Message.tsx'
-
+import { useState } from "react";
+import SideNavbar from "./components/SideNavbar";
+import TopBar from "./components/TopBar";
+import Dashboard from "./components/Dashboard";
 function App() {
-  return <div> <Message /> </div>
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <div className="flex min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+      <SideNavbar open={sidebarOpen} setOpen={setSidebarOpen} />
+
+      <div className="flex-1 flex flex-col">
+        <TopBar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+
+        <div>
+          <Dashboard />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;
